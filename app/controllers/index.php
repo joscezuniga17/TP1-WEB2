952931f3fe7ded206810b24723d0cd8f4abb7d53
Tomiiepa15 <tomasiepaiel5@gmail.com>
2025-10-21 15:47:55 -0300

    Update index.php

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,1 +1,16 @@
+<?php
+include('conexion.php');
+$query = "SELECT * FROM autor";
+$resultado = mysqli_query($conexion, $query);
+?>
 
+<h1>Listado de Autores</h1>
+<ul>
+<?php while($a = mysqli_fetch_assoc($resultado)): ?>
+    <li>
+        <a href="autor.php?id_autor=<?= $a['id_autor'] ?>">
+            <?= $a['nombre'] ?>
+        </a> (<?= $a['nacionalidad'] ?>)
+    </li>
+<?php endwhile; ?>
+</ul>
