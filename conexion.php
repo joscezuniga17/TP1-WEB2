fd2f5ff33bb94b6f75ccf2e1abb5c6adfb03a9f1
Tomiiepa15 <tomasiepaiel5@gmail.com>
2025-10-21 12:33:04 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,4 +5,13 @@ $conexion = mysqli_connect("localhost", "root", "", "lista de reproducción musi
 if (!$conexion) {
     die("Error al conectar con la base de datos: " . mysqli_connect_error());
 }
+    $host = "localhost";
+$db = "playlist musical"; // Nombre de tu base de datos
+$user = "root";
+$pass = ""; // Tu contraseña de MySQL/MariaDB
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
 ?>
