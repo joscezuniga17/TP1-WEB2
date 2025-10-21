1e3716ba91b62276526a3bc718fc456963cc7074
Tomiiepa15 <tomasiepaiel5@gmail.com>
2025-10-21 15:41:10 -0300

    Update autor.php

--- app/models/autor.php
+++ app/models/autor.php
@@ -1,1 +1,21 @@
+<?php
+incluir('conexión.php');
+$id_autor=$_CONSEGUIR['id_autor'];
+$consulta="SELECCIONA canciones.*, autor.nombre AS nombre_autor
+          DE canciones
+          INNER JOIN autor ON canciones.id_autor = autor.id_autor
+          DONDE canciones.id_autor =$id_autor";
+$resultado=consulta mysqli($conexión,$consulta);
+?>
 
+<h1>Canciones del autor</h1>
+<ul>
+<?php mientras($do=asociación de búsqueda de mysqli($resultado)):?>
+  <li>
+    <fuerte><?= $do['nombre_cancion']?></strong><br>
+    Álbum:<?= $do['álbum']?><br>
+    Duración:<?= $do['duración']?>mín.
+    Autor:<?= $do['nombre_autor']?>
+  </li>
+<?php mientras tanto;?>
+</ul>
